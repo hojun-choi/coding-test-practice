7d060aa0dbec1599ca50d94e63239e454cb3d74f
note <dhgyd0221@gmail.com>
2026-05-08 00:21:43 +0900

    W1D2 추가

--- problems/basic/array-string-hash/easy/longest-common-prefix.py
+++ problems/basic/array-string-hash/easy/longest-common-prefix.py
@@ -12,20 +12,40 @@ Study Plan:
   - Check string comparison and early stop logic.
 
 Idea:
-- Not written yet.
+- 최소길이 찾고 그 길이만큼 비교하면서 다르면 바로 리턴하는 방식으로 풀이.
 
 Code:
   '''
+  class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+        len_min=len(strs[0])
+        for st in strs:
+            if len(st) < len_min:
+                len_min = len(st)
+        if len_min==0:
+            return ""
+        for n in range(len_min):
+            word = strs[0][n]
+            for st in strs:
+                if st[n] != word:
+                    if n == 0:
+                        return ""
+                    return st[0:n]
+
+        return st[0:len_min]
+
   '''
 
 Time Complexity:
-- Not written yet.
+- O(n*m) where n is the number of strings and m is the length of the shortest string.
 
 Space Complexity:
-- Not written yet.
+- O(1) if we don't consider the output string, otherwise O(m) for the output string.
 
 Status:
-- Unsolved
+- Solved
 
 Mistake:
 - None yet.
